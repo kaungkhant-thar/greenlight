bd6a0adfde8c1c7533a2edb0cabbd8c3b734a4d7
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2025-10-04 16:52:48 +0700

    feat: added errors json response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,15 +6,16 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	env := envelope{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 
 }
